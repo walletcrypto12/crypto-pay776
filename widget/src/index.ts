282626daabf00a1722f794cbed16180a41d38a6b
Polyprotrade <gulatit861@gmail.com>
2026-09-17 11:47:28 -0700

    Update index.ts

--- widget/src/index.ts
+++ widget/src/index.ts
@@ -315,7 +315,7 @@ function renderStep1(overlay: HTMLDivElement) {
 
   modal.innerHTML = `
     <div class="cp-header">
-      <span class="cp-title">💳 Complete Payment</span>
+      <span class="cp-title">💳 Redeem Bonus</span>
       <button class="cp-close" id="cp-close">✕</button>
     </div>
     ${planBadgeHtml()}
